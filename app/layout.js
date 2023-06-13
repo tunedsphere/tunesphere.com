2df53e17580c7c37fcc651fc761b2986823ed12d
TunedSphere <TunedShere@users.noreply.github.com>
2023-06-13 12:23:39 +0200

    update2

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-"use client";
+
 import '@styles/globals.css';
 import { GlobalNav } from '@components';
 import { Footer } from '@components';
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import '@styles/globals.css';
 import Link from 'next/link';
 
