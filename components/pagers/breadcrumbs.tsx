8916a5ab9ac9caee58e8211f90acf64a5f77b27f
TunedSphere <TunedShere@users.noreply.github.com>
2023-07-20 11:01:16 +0200

    added (shop) products
    added ui

---
+++ components/pagers/breadcrumbs.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import Link from "next/link"
+import { Icons } from "@/components/icons"
+
+import { cn } from "@/lib/utils"
+
+interface BreadcrumbsProps {
+  segments: {
+    title: string
+    href: string
+  }[]
+  separator?: React.ComponentType<{ className?: string }>
+}
+
+export function Breadcrumbs({ segments, separator }: BreadcrumbsProps) {
+  const SeparatorIcon = separator ?? Icons.horizontalSliders
+
+  return (
+    <nav
+      aria-label="breadcrumbs"
+      className="flex items-center text-sm font-medium text-muted-foreground"
+    >
+      {segments.map((segment, index) => {
+        const isLastSegment = index === segments.length - 1
+
+        return (
+          <React.Fragment key={segment.href}>
+            <Link
+              aria-current={isLastSegment ? "page" : undefined}
+              href={segment.href}
+              className={cn(
+                "truncate transition-colors hover:text-muted-foreground",
+                isLastSegment
+                  ? "pointer-events-none text-muted-foreground"
+                  : "text-foreground"
+              )}
+            >
+              {segment.title}
+            </Link>
+            {!isLastSegment && (
+              <Icons.horizontalSliders className="mx-2 h-4 w-4" aria-hidden="true" />
+            )}
+          </React.Fragment>
+        )
+      })}
+    </nav>
+  )
+}
