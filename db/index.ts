9762ae859b91694a0bef8b95bdeac37d3f8e33a2
TunedSphere <TunedShere@users.noreply.github.com>
2023-07-21 10:47:11 +0200

    dotenv confing

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
-import { env } from "@/env.mjs"
+import 'dotenv/config'
 import { connect } from "@planetscale/database"
 import { drizzle } from "drizzle-orm/planetscale-serverless"
 
@@ -6,8 +6,8 @@ import * as schema from "./schema"
 
 // Create the connection
 const connection = connect({
-  host: env["DATABASE_HOST"],
-  username: env["DATABASE_USERNAME"],
-  password: env["DATABASE_PASSWORD"],
+  host: process.env["DATABASE_HOST"],
+  username: process.env["DATABASE_USERNAME"],
+  password: process.env["DATABASE_PASSWORD"],
 })
 export const db = drizzle(connection, { schema })
