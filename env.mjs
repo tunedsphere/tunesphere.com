3b63cbe3bf078f15940c1be161ac2eafb0798c75
TunedSphere <TunedShere@users.noreply.github.com>
2023-07-21 11:25:59 +0200

    Update env.mjs

--- env.mjs
+++ env.mjs
@@ -10,7 +10,6 @@ export const env = createEnv({
     DATABASE_HOST: z.string(),
     DATABASE_USERNAME: z.string(),
     DATABASE_PASSWORD: z.string(),
-    RESEND_API_KEY: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     CLERK_SECRET_KEY: z.string(),
 
@@ -34,7 +33,7 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     DATABASE_HOST: process.env.DATABASE_HOST,
     DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-    DATABASE_PASSWORD:process.env.ATABASE_PASSWORD,
+    DATABASE_PASSWORD:process.env.DATABASE_PASSWORD,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
