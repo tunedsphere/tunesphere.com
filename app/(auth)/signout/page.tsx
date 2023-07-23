3550bd799c7c1e0f4d447baeb17796cd6b8f0820
TunedSphere <TunedShere@users.noreply.github.com>
2023-07-23 12:41:58 +0200

    gap header

--- app/(auth)/signout/page.tsx
+++ app/(auth)/signout/page.tsx
@@ -6,7 +6,7 @@ import { Card } from "@components/ui/card"
 
 export default function SignOutPage() {
   return (
-    <Shell layout="auth" className="max-w-xs gap-2 border border-white bg-accent1 p-16 rounded-md">
+    <Shell layout="auth" className="max-w-xs gap-2 sm:border border-white bg-accent1 p-16 rounded-md">
       <Header
         title="Sign out"
         description="Are you sure you want to sign out?"
--- components/layouts/site-header.tsx
+++ components/layouts/site-header.tsx
@@ -47,7 +47,7 @@ export function SiteHeader() {
       <div id="globalnav-content" className="relative">
         <nav className="bg-backgroundNavbarTop z-9999 items-center md:px-8">
           <div className="py-2 navbar-container h-[--headerHeight] flex flex-between justify-between mx-auto">
-          <div className="sm:w-1/3 sm:visible invisible relative items-center sm:gap-2 gap-1 flex flex-row justify-start px-4">    
+          <div className="sm:w-1/3 sm:visible invisible hidden relative items-center sm:gap-2 gap-[2px] sm:flex flex-row justify-start px-4">    
              <a>LOGO</a>
             </div>
             <div className="w-4/6 flex sm:justify-center items-center left-0 px-4">
@@ -57,7 +57,7 @@ export function SiteHeader() {
                 </h2>
               </Link>
             </div>
-            <div className="sm:w-1/3 w-2/6 relative items-center sm:gap-2 gap-1 flex flex-row justify-end px-4">
+            <div className="sm:w-1/3 w-2/6 relative items-center sm:gap-2 gap-[1px] flex flex-row justify-end px-4">
             <CartSheet />
               <div  className='hidden sm:block'>
               <ThemeToggle/>
