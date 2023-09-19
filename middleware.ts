71911e5da745e879457edef21d5529c4549466ee
TunedSphere <TunedShere@users.noreply.github.com>
2023-09-19 16:31:46 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,62 @@
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server"
+import { type UserRole } from "@/types"
+import { clerkClient } from "@clerk/nextjs"
 
-export default async function middleware(req) {
-  if (req.url !== "/") {
-    return NextResponse.redirect("https://tunedsphere.com/");
-  }
-  return NextResponse.next();
-}
+
+export default authMiddleware({
+   // Public routes are routes that don't require authentication
+  publicRoutes: [
+    "/",
+    "/signin(.*)",
+    "/signup(.*)",
+    "/sso-callback(.*)",
+    "/api(.*)",
+    "/shop(.*)",
+    "/shop/categories(.*)",
+    "/shop/products(.*)",
+    "/shop/product(.*)",
+    "/music(.*)", 
+    "/genres(.*)", 
+    "/festivals(.*)", 
+    "/artsits(.*)", 
+    "/albums(.*)", 
+    "/uipage(.*)",
+  ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next()
+    }
+
+    const url = new URL(req.nextUrl.origin)
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/signin"
+      return NextResponse.redirect(url)
+    }
+
+    // Set the user's role to user if it doesn't exist
+    const user = await clerkClient.users.getUser(auth.userId)
+
+    if (!user) {
+      throw new Error("User not found.")
+    }
+
+    // If the user doesn't have a role, set it to user
+    if (!user.privateMetadata.role) {
+      await clerkClient.users.updateUserMetadata(auth.userId, {
+        privateMetadata: {
+          role: "user" satisfies UserRole,
+        },
+      })
+    }
+  },
+})
 
 export const config = {
-  api: {
-    bodyParser: false,
-  },
+
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api)(.*)"],
 };
