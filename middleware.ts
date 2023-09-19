9a80ccf42a4ffbb7eaeb17ecc7f1c1e669bfca66
TunedSphere <TunedShere@users.noreply.github.com>
2023-09-19 13:13:40 +0200

    MiddleWare for COming Soon

--- middleware.ts
+++ middleware.ts
@@ -1,62 +1,14 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server"
-import { type UserRole } from "@/types"
-import { clerkClient } from "@clerk/nextjs"
+import { NextResponse } from "next/server";
 
-
-export default authMiddleware({
-   // Public routes are routes that don't require authentication
-  publicRoutes: [
-    "/",
-    "/signin(.*)",
-    "/signup(.*)",
-    "/sso-callback(.*)",
-    "/api(.*)",
-    "/shop(.*)",
-    "/shop/categories(.*)",
-    "/shop/products(.*)",
-    "/shop/product(.*)",
-    "/music(.*)", 
-    "/genres(.*)", 
-    "/festivals(.*)", 
-    "/artsits(.*)", 
-    "/albums(.*)", 
-    "/uipage(.*)",
-  ],
-  async afterAuth(auth, req) {
-    if (auth.isPublicRoute) {
-      //  For public routes, we don't need to do anything
-      return NextResponse.next()
-    }
-
-    const url = new URL(req.nextUrl.origin)
-
-    if (!auth.userId) {
-      //  If user tries to access a private route without being authenticated,
-      //  redirect them to the sign in page
-      url.pathname = "/signin"
-      return NextResponse.redirect(url)
-    }
-
-    // Set the user's role to user if it doesn't exist
-    const user = await clerkClient.users.getUser(auth.userId)
-
-    if (!user) {
-      throw new Error("User not found.")
-    }
-
-    // If the user doesn't have a role, set it to user
-    if (!user.privateMetadata.role) {
-      await clerkClient.users.updateUserMetadata(auth.userId, {
-        privateMetadata: {
-          role: "user" satisfies UserRole,
-        },
-      })
-    }
-  },
-})
+export default async function middleware(req) {
+  if (req.url !== "/") {
+    return NextResponse.redirect("/");
+  }
+  return NextResponse.next();
+}
 
 export const config = {
-
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api)(.*)"],
+  api: {
+    bodyParser: false,
+  },
 };
