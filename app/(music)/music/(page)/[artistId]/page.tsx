11b5f4ff505ed5621f20b089d054243681b7182a
TunedSphere <jonathan.avison@protonmail.com>
2023-12-18 18:28:24 +0100

    Fixed Music Tabs

--- app/(music)/music/(page)/[artistId]/page.tsx
+++ app/(music)/music/(page)/[artistId]/page.tsx
@@ -1,15 +1,15 @@
-"use client";
-import "@/styles/globals.css";
+'use client'
+import '@/styles/globals.css'
 
-import Link from "next/link";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import * as React from "react";
-import { useState } from "react";
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import * as React from 'react'
+import { useState } from 'react'
 
-import { albums } from "@/public/albumsData";
+import { albums } from '@/public/albumsData'
 
-import { Icon } from "@/components/icon";
+import { Icon } from '@/components/icon'
 
 // export const metadata: Metadata = {
 //   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
@@ -18,30 +18,30 @@ import { Icon } from "@/components/icon";
 // };
 interface ArtistPageProps {
   params: {
-    ArtistId: string;
-  };
+    ArtistId: string
+  }
 }
 
 export default function ArtistPage({ params }: ArtistPageProps) {
-  const ArtistId = String(params.ArtistId);
-  const [gridView, setGridView] = useState(true);
+  const ArtistId = String(params.ArtistId)
+  const [gridView, setGridView] = useState(true)
 
   const toggleGridMode = () => {
-    setGridView(true); // Set to grid view
-  };
+    setGridView(true) // Set to grid view
+  }
 
   const toggleListMode = () => {
-    setGridView(false); // Set to list view
-  };
+    setGridView(false) // Set to list view
+  }
 
   return (
     <>
-      <div className="flex justify-end  items-center max-w-6xl mx-auto bg-background px-8 font-bold py-2">
-        <div className="flex align-middle mr-[160px]">
+      <div className="mx-auto flex  max-w-6xl items-center justify-end bg-background px-8 py-2 font-bold">
+        <div className="mr-[160px] flex align-middle">
           <Button
             variant="ghost"
             className={`rounded-full transition-colors hover:bg-muted/50 hover:text-foreground/80 ${
-              !gridView ? "" : "bg-muted/70"
+              !gridView ? '' : 'bg-muted/70'
             }`}
             onClick={toggleGridMode}
           >
@@ -50,8 +50,8 @@ export default function ArtistPage({ params }: ArtistPageProps) {
 
           <Button
             variant="ghost"
-            className={`ml-2 rounded-full hover:bg-muted/50 transition-colors hover:text-foreground/80 ${
-              gridView ? "" : "bg-muted/70"
+            className={`ml-2 rounded-full transition-colors hover:bg-muted/50 hover:text-foreground/80 ${
+              gridView ? '' : 'bg-muted/70'
             }`}
             onClick={toggleListMode}
           >
@@ -60,29 +60,29 @@ export default function ArtistPage({ params }: ArtistPageProps) {
         </div>
       </div>
       <div
-        className="flex justify-center items-center"
+        className="flex items-center justify-center"
         style={{
-          backgroundImage: "/images/product-placeholder.webp",
+          backgroundImage: '/images/product-placeholder.webp',
         }}
       >
-        <div className="container mx-auto max-w-6xl flex pb-24 backdrop-blur-sm bg-background px-8">
-          <div className="flex-grow flex-wrap @container pr-12">
+        <div className="container mx-auto flex max-w-6xl bg-background px-8 pb-24 backdrop-blur-sm">
+          <div className="flex-grow flex-wrap pr-12 @container">
             {gridView ? (
-              <div className="grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-4 justify-around">
+              <div className="grid grid-flow-row-dense grid-cols-1 justify-around gap-x-8 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {albums.map((album) => (
                   <div className="flex flex-col gap-y-2">
                     <Link href="/albums">
                       <Image
                         key={`album.id_image`}
-                        src={"/images/product-placeholder.webp"}
+                        src={'/images/product-placeholder.webp'}
                         alt="album.title"
-                        className="cursor-pointer aspect-square rounded-md"
+                        className="aspect-square cursor-pointer rounded-md"
                         width={300}
                         height={300}
                         loading="lazy"
                       />
                     </Link>
-                    <p className="text-base text-left font-bold break-words">
+                    <p className="break-words text-left text-base font-bold">
                       Title
                     </p>
                   </div>
@@ -91,19 +91,19 @@ export default function ArtistPage({ params }: ArtistPageProps) {
             ) : (
               albums.map((album) => (
                 <div className="-mx-4 px-4 pb-20">
-                  <div className="@container grid grid-rows-[min-content_1fr] gap-4 grid-cols-2 @xs:grid-cols-4 @md:grid-cols-5 @lg:grid-cols-8 @6xl:grid-cols-6 @7xl:grid-cols-7 @8xl:grid-cols-8 @9xl:grid-cols-9">
-                    <div className="@md:col-span-1 @xl:row-span-2 @lg:row-span-2 @lg:col-span-2 @6xl:col-span-2">
+                  <div className="grid grid-cols-2 grid-rows-[min-content_1fr] gap-4 @container @xs:grid-cols-4 @md:grid-cols-5 @lg:grid-cols-8 @6xl:grid-cols-6 @7xl:grid-cols-7 @8xl:grid-cols-8 @9xl:grid-cols-9">
+                    <div className="@md:col-span-1 @lg:col-span-2 @lg:row-span-2 @xl:row-span-2 @6xl:col-span-2">
                       <Image
                         width={500}
                         height={500}
                         src={album.image}
                         alt={album.title}
-                        className="cursor-pointer border border-muted aspect-square rounded-md"
+                        className="aspect-square cursor-pointer rounded-md border border-muted"
                       ></Image>
                     </div>
-                    <div className="col-start-2 @lg:col-start-3 col-span-3 @lg:col-span-5 @2xl:pl-4">
+                    <div className="col-span-3 col-start-2 @lg:col-span-5 @lg:col-start-3 @2xl:pl-4">
                       <Link href={`label/${album.id}`}>
-                        <h3 className="cursor-pointer  text-3xl @2xl:text-6xl font-semibold ">
+                        <h3 className="cursor-pointer  text-3xl font-semibold @2xl:text-6xl ">
                           {album.title}
                         </h3>
                       </Link>
@@ -113,33 +113,33 @@ export default function ArtistPage({ params }: ArtistPageProps) {
                         <p className="text-textlow">{album.genre}</p>
                       </div>
                     </div>
-                    <div className="@lg:col-start-1 col-span-full">
+                    <div className="col-span-full @lg:col-start-1">
                       <div className="grid-flow-col">
-                        <div className="flex justify-between text-base text-textlow border-b py-2 mb-4 border-muted">
+                        <div className="mb-4 flex justify-between border-b border-muted py-2 text-base text-textlow">
                           <div className="px-4">
-                            <span className="">#</span>{" "}
+                            <span className="">#</span>{' '}
                             <span className="mx-1"> Title</span>
                           </div>
                         </div>
                         {album.tracklist.map((track, index) => (
                           <div
                             key={index}
-                            className="flex align-center items-center justify-between text-base text-texthigh py-2 border-muted cursor-pointer hover:bg-muted/50 rounded-md"
+                            className="align-center flex cursor-pointer items-center justify-between rounded-md border-muted py-2 text-base text-texthigh hover:bg-muted/50"
                           >
                             <div className="flex-start">
                               <span className="px-4">
-                                {track.track}{" "}
-                                <span className="text-sm mx-1"> - </span>{" "}
+                                {track.track}{' '}
+                                <span className="mx-1 text-sm"> - </span>{' '}
                               </span>
                             </div>
-                            <div className="flex flex-col flex-start grow">
+                            <div className="flex-start flex grow flex-col">
                               {track.title}
 
                               <span className="text-sm text-textlow">
                                 Track Artists
                               </span>
                             </div>
-                            <div className="flex flex-end">
+                            <div className="flex-end flex">
                               <span className="px-4">{track.length}</span>
                             </div>
                           </div>
@@ -152,17 +152,17 @@ export default function ArtistPage({ params }: ArtistPageProps) {
             )}
           </div>
 
-          <div className="shrink-0 w-[120px] ml-auto flex flex-col gap-2">
+          <div className="ml-auto flex w-[120px] shrink-0 flex-col gap-2">
             <Image
               key={`artist.id_image`}
-              src={"/images/product-placeholder.webp"}
+              src={'/images/product-placeholder.webp'}
               alt="album.title"
-              className="cursor-pointer aspect-square rounded-md"
+              className="aspect-square cursor-pointer rounded-md"
               width={120}
               height={120}
               loading="lazy"
             />
-            <p id="band-name-location" className="break-words flex flex-col">
+            <p id="band-name-location" className="flex flex-col break-words">
               <span className="font-bold">ArtistNameistoooooooooooooooooo</span>
               <span className="text-sm">Country</span>
             </p>
@@ -171,5 +171,5 @@ export default function ArtistPage({ params }: ArtistPageProps) {
         </div>
       </div>
     </>
-  );
+  )
 }
--- app/(music)/music/(page)/layout.tsx
+++ app/(music)/music/(page)/layout.tsx
@@ -1,33 +1,27 @@
-"use client";
-import "@/styles/globals.css";
-import * as React from "react";
-import { useState } from "react";
+'use client'
+import '@/styles/globals.css'
+import * as React from 'react'
+import { useState } from 'react'
 
-import { MusicTabs } from "@/components/pagers/music-tabs";
+import { MusicTabs } from '@/components/pagers/music-tabs'
 
 interface MusicLayoutPageProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default function MusicLayoutPage({ children }: MusicLayoutPageProps) {
-  const [selectedTab, setSelectedTab] = useState("Home");
-
-  const handleTabChange = (tab: string) => {
-    setSelectedTab(tab);
-  };
-
   return (
     <section className="mt-[var(--headerHeight)]">
       <div className="">
         <div className="relative mx-auto flex items-center justify-center">
           <div className="inset-x-0 mx-auto flex justify-center gap-2 overflow-x-hidden">
-            <MusicTabs onTabChange={handleTabChange} />
+            <MusicTabs />
           </div>
         </div>
-        <main className="flex-1 border-t border-b border-muted @container">
+        <main className="flex-1 border-b border-t border-muted @container">
           {children}
         </main>
       </div>
     </section>
-  );
+  )
 }
